32c1fe51d25e136a75193a35072c840c912f2066
AnkitPaswan <ankitpaswan192@gmail.com>
2025-07-23 12:30:06 +0530

    minor change

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -75,7 +75,7 @@ router.post("/", async (req, res) => {
     line_items: lineItems,
     mode: "payment",
     shipping_address_collection: { allowed_countries: ["IN"] },
-    success_url: `${process.env.FRONTEND_URL}/successpage`,
+    success_url: `${process.env.FRONTEND_URL}/?payment=success`,
     cancel_url: `${process.env.FRONTEND_URL}/cancelpage`,
   });
   res.json({ id: session.id });
